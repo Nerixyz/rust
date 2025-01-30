9620038cd19a499b611cfd19d408a22e91441e86
Nerixyz <nerixdev@outlook.de>
2025-01-30 18:58:19 +0100

    chore: debug

--- build-it.sh
+++ build-it.sh
@@ -9,6 +9,7 @@ cd "$(dirname "$0")"
     --set llvm.link-shared=true \
     --set rust.lld=false \
     --set rust.use-lld=false \
+    --set rust.debug=true \
     --set target.x86_64-unknown-linux-gnu.llvm-config=/usr/bin/llvm-config-18
 
 ./x.py build
