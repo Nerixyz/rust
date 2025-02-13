1a425433c8f8f76bf8181c49e8fa70fccdc09460
Nerixyz <nerixdev@outlook.de>
2025-02-13 10:08:50 +0100

    level

--- build-it.sh
+++ build-it.sh
@@ -10,6 +10,7 @@ cd "$(dirname "$0")"
     --set rust.lld=false \
     --set rust.use-lld=false \
     --set rust.debug=true \
+    --set rust.debuginfo-level=2 \
     --set target.x86_64-unknown-linux-gnu.llvm-config=/usr/bin/llvm-config-18
 
 ./x.py build
